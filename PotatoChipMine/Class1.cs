4062688a21569aae19d74aa79162e5547b8d3910
Pete Christinzio <peter.christinzio@gmail.com>
2018-10-10 20:12:28 -0400

    Initial Push

---
+++ PotatoChipMine/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PotatoChipMine
+{
+    public class Class1
+    {
+    }
+}
---
+++ PotatoChipMineTests/ChipDiggerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Text;
+using Shouldly;
+using Xunit;
+
+namespace PotatoChipMineTests
+{
+    public class ChipDiggerTests
+    {
+        [Fact]
+        public void ChipDiggerDigTest()
+        {
+            var chipDigger = new ChipDigger();
+            var scoop = chipDigger.Dig();
+            scoop.Chips.ShouldBeGreaterThan(0);
+        }
+    }
+
+    public class ChipDigger
+    {
+        public Scoop Dig()
+        {
+            return new Scoop(){Chips = 1};
+        }
+    }
+
+    public class Scoop
+    {
+        public int Chips { get; set; }
+    }
+}
